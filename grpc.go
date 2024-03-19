0fc757396ebe812945774a312ab8d4b0c3f9abcf
justjack1521 <henryt1521@hotmail.com>
2024-03-19 23:32:23 +1100

    Added config for grpc

---
+++ grpc.go
@@ -0,0 +1,56 @@
+package mevconn
+
+import (
+	"fmt"
+	"github.com/justjack1521/mevconn/internal/env"
+)
+
+type ServiceName string
+
+const (
+	GAMESERVICENAME   ServiceName = "GAME"
+	AUTHSERVICENAME   ServiceName = "AUTH"
+	SOCIALSERVICENAME ServiceName = "SOCIAL"
+	RANKSERVICENAME   ServiceName = "RANK"
+
+	grpcHostEnvKey = "GRPC_HOST_"
+	grpcPortEnvKey = "GRPC_PORT_"
+)
+
+var (
+	errBuildingGrpcServiceConfig = func(name ServiceName, err error) error {
+		return fmt.Errorf("error when building %s grpc service config: %w", name, err)
+	}
+)
+
+type GrpcService interface {
+	ConnectionString() string
+}
+
+type grpcService struct {
+	host string
+	port string
+}
+
+func (g grpcService) ConnectionString() string {
+	return fmt.Sprintf("%s:%s", g.host, g.port)
+}
+
+func CreateGrpcServiceConfig(name ServiceName) (GrpcService, error) {
+
+	host, err := env.MustGetEnvironmentVariable(grpcHostEnvKey + string(name))
+	if err != nil {
+		return nil, errBuildingGrpcServiceConfig(name, err)
+	}
+
+	port, err := env.MustGetEnvironmentVariable(grpcPortEnvKey + string(name))
+	if err != nil {
+		return nil, errBuildingGrpcServiceConfig(name, err)
+	}
+
+	return grpcService{
+		host: host,
+		port: port,
+	}, nil
+
+}
